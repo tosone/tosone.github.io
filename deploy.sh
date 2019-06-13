f0696f7e541dcc333d97210f1674568a278db438
Tosone <i@tosone.cn>
2019-06-13 20:17:03 +0800

    Add new post Thu Jun 13 20:17:03 CST 2019

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+hugo
+
+cd public
+
+git add .
+git commit -m "Rebuilding site `date`"
+git push origin master --force-with-lease
+
+cd ..
+
+git add .
+git commit -m "Add new post `date`"
+git push origin source --force-with-lease
